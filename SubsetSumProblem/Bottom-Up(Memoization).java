49065d5665bc46b4d8a7ed7488d7cc1b035dcf0f
Simran Srivastava <52354644+simransrivastava01@users.noreply.github.com>
2021-01-20 21:11:10 +0530

    Subset Sum Problem

---
+++ SubsetSumProblem/Bottom-Up(Memoization).java
@@ -0,0 +1,43 @@
+/* Subset Sum Problem (Bottom-Up) */
+
+import java.util.*;
+
+class GFG {
+    public static boolean isSubsetSum(int arr[], int n, int sum)
+    {
+        if(n==0)
+          return false;
+        
+        if(sum==0)
+          return true;
+          
+        boolean dp[][] = new boolean[n+1][sum+1];
+        
+        if(dp[n][sum]!=false)
+        {
+            return dp[n][sum];
+        }
+        
+        if(arr[n-1]<=sum)
+          {
+            dp[n][sum] = isSubsetSum(arr,n-1,sum-arr[n-1]) || isSubsetSum(arr,n-1,sum);
+          }
+        else
+          {
+            dp[n][sum] = isSubsetSum(arr,n-1,sum);
+          }
+        return dp[n][sum];
+    }
+	public static void main (String[] args) {
+	   Scanner obj = new Scanner(System.in);
+	   int n=obj.nextInt();
+	   int arr[] = new int[n];
+	   for(int i=0;i<n;i++)
+	   {
+	       arr[i]=obj.nextInt();
+	   }
+	   int sum=obj.nextInt();
+	   
+	   System.out.print(isSubsetSum(arr,n,sum));
+	}
+}
---
+++ SubsetSumProblem/Top-Down.java
@@ -0,0 +1,48 @@
+/* Subset Sum Problem (Top-Down) */
+
+import java.util.*;
+
+class GFG {
+    public static boolean isSubsetSum(int arr[], int n, int sum)
+    {
+        boolean dp[][] = new boolean[n+1][sum+1];
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=true;
+        }
+        
+        for(int j=1;j<=sum;j++)
+        {
+            dp[0][j]=false;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+	public static void main (String[] args) {
+	   Scanner obj = new Scanner(System.in);
+	   int n=obj.nextInt();
+	   int arr[] = new int[n];
+	   for(int i=0;i<n;i++)
+	   {
+	       arr[i]=obj.nextInt();
+	   }
+	   int sum=obj.nextInt();
+	   
+	   System.out.print(isSubsetSum(arr,n,sum));
+	}
+}
